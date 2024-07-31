90df002b3113ec26bb2081985949214c1ada722a
Eisha313 <eishakamran313@gmail.com>
2024-07-31 21:13:53 +0500

    kill me plea33333333333333333333333

--- src/app/viewuser/[Id]/edit/page.tsx
+++ src/app/viewuser/[Id]/edit/page.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import Sidebar from '../../../components/sidebarDashboard';
-import Header from '../../../components/header';
-import QuizManager from '../../../components/verbalcomponent';
-import Link from 'next/link';
+// import React from 'react';
+// import Sidebar from '../../../components/sidebarDashboard';
+// import Header from '../../../components/header';
+// import QuizManager from '../../../components/verbalcomponent';
+// import Link from 'next/link';
 
-// import ManageUsers from '../components/viewuser';
-import EditUser from '../../../components/edituser'
+// // import ManageUsers from '../components/viewuser';
+// import EditUser from '../../../components/edituser'
 
-const VerbalPage = ({params}:{
-    params:{
-        Id:string
-    }
-}) => {
+// const VerbalPage = ({params}:{
+//     params:{
+//         Id:string
+//     }
+// }) => {
 
-    const {Id} = params;
-    console.log("userId",Id)
-  return (
-    <div className="flex h-screen">
-      <Sidebar />
+//     const {Id} = params;
+//     console.log("userId",Id)
+//   return (
+//     <div className="flex h-screen">
+//       <Sidebar />
       
-      <div className="flex-1 flex flex-col">
-        <Header />  
+//       <div className="flex-1 flex flex-col">
+//         <Header />  
         
         
-        <main className="flex-1 p-4 bg-white">
-          <EditUser id={Id}/>
-        </main>
-      </div>
-    </div>
-  );
-};
+//         <main className="flex-1 p-4 bg-white">
+//           <EditUser id={Id}/>
+//         </main>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default VerbalPage;
+// export default VerbalPage;
