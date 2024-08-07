fa2884b5808c8d6f3c507109ebe812710080e96f
Eisha313 <eishakamran313@gmail.com>
2024-08-07 21:28:50 +0500

    7Aug2

--- src/app/addaviator/page.tsx
+++ src/app/addaviator/page.tsx
@@ -1,8 +1,22 @@
-import AddAviatorForm from "../components/addaviatoronsave";
+// // src/app/addaviator/page.tsx
+// import AddAviatorForm from "../components/addaviatoronsave";
 
+// export default function ForgotPasswordPage() {
+//   return (
+//     <AddAviatorForm />
+//   );
+// }
+'use client';
 
-export default function ForgotPasswordPage() {
-  return (
-    <AddAviatorForm ></AddAviatorForm>
-  );
-}
+import React from 'react';
+import AddAviatorForm from '../components/addaviatoronsave';
+import { useSearchParams } from 'next/navigation';
+
+const EditUser: React.FC = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id'); // Replace 'id' with the actual query parameter name
+
+  return <AddAviatorForm id={id || ''} />;
+};
+
+export default EditUser;
