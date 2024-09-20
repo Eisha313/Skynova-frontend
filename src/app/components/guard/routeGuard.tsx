43f790822468c2c5dce3ef2bafff10d9e89bf504
Eisha313 <eishakamran313@gmail.com>
2024-09-20 21:14:21 +0500

    20Sep100

--- src/app/components/guard/routeGuard.tsx
+++ src/app/components/guard/routeGuard.tsx
@@ -56,7 +56,7 @@ const RouteGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
         if (pathname === '/login' || pathname === '/signup') {
           if (userRole === 'Aviator') {
-            router.push('/userRender/viewCommunityQuestions');
+            router.push('/');
           } else if (userRole === 'Admin') {
             router.push('/dashboard');
           } else {
