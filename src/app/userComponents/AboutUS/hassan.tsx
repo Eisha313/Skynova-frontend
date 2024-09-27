38daf5af25085369a8d41276aba5fc1148c746d4
Eisha313 <eishakamran313@gmail.com>
2024-09-27 21:44:25 +0500

    26september55

--- src/app/userComponents/AboutUS/hassan.tsx
+++ src/app/userComponents/AboutUS/hassan.tsx
@@ -7,23 +7,21 @@ const MyProfileee = () => {
       <div className="w-full lg:w-6/12 lg:ml-6 mt-6 lg:mt-0 flex flex-col justify-center">
         <h2 className="text-3xl font-bold">MY PROFILE</h2>
         <p className="text-lg mt-4">
-        A creative and skilled 3D designer with expertise in Blender and Unity, dedicated to bringing imaginative concepts to life through stunning visuals and engaging interactive experiences. My passion for design and technology drives me to create high-quality 3D models, animations, and environments that captivate users and enhance gameplay.
+          A creative and skilled 3D designer with expertise in Blender and Unity, dedicated to bringing imaginative concepts to life through stunning visuals and engaging interactive experiences. My passion for design and technology drives me to create high-quality 3D models, animations, and environments that captivate users and enhance gameplay.
 
-With a strong foundation in 3D modeling and animation, I excel at transforming ideas into realistic assets for games, simulations, and visualizations. Proficient in Blenders advanced features, I can produce detailed characters, intricate environments, and dynamic animations that align with the project's artistic vision. My keen eye for detail ensures that every model is optimized for performance and aesthetics, striking a balance that enhances the user experience.
+          With a strong foundation in 3D modeling and animation, I excel at transforming ideas into realistic assets for games, simulations, and visualizations. Proficient in Blender&#39;s advanced features, I can produce detailed characters, intricate environments, and dynamic animations that align with the project&#39;s artistic vision. My keen eye for detail ensures that every model is optimized for performance and aesthetics, striking a balance that enhances the user experience.
         </p>
       </div>
       <div className="w-full lg:w-6/12 flex-shrink-0">
         <img
-          src="/MyProfileImage.png" 
+          src="/MyProfileImage.png"
           alt="My Profile"
           className="w-full h-auto object-cover rounded-md shadow-md"
         />
       </div>
-
-     
-      
     </div>
   );
 };
+
 
 export default MyProfileee;
