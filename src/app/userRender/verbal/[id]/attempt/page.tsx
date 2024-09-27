ff1196ca5f47101ac0a28b92262c9abad6bb502e
Eisha313 <eishakamran313@gmail.com>
2024-09-27 22:26:51 +0500

    26september99

--- src/app/userRender/verbal/[id]/attempt/page.tsx
+++ src/app/userRender/verbal/[id]/attempt/page.tsx
@@ -1,29 +1,61 @@
+// import React from 'react';
+// import Header from '@/app/components/header';
+// import Sidebar from '@/app/components/sidebarDashboard';
+
+
+
+// import QuizAttempt from '@/app/userComponents/verbal/QuizAttempt';
+
+// const EditPage = ({ params }: { params: { id: string } }) => {
+//     const { id } = params;
+//     console.log('userTitle', id);
+  
+//     return (
+//       <div className="flex h-screen">
+//         <Sidebar />
+  
+//         <div className="flex-1 flex flex-col">
+//           <Header />
+  
+//           <main className="flex-1 p-4 bg-white">
+//             <QuizAttempt id={id} />
+//           </main>
+//         </div>
+//       </div>
+//     );
+//   };
+  
+//   export default EditPage;
+  // EditPage component (page.tsx)
 import React from 'react';
 import Header from '@/app/components/header';
 import Sidebar from '@/app/components/sidebarDashboard';
-
-
-
 import QuizAttempt from '@/app/userComponents/verbal/QuizAttempt';
 
 const EditPage = ({ params }: { params: { id: string } }) => {
-    const { id } = params;
-    console.log('userTitle', id);
-  
-    return (
-      <div className="flex h-screen">
-        <Sidebar />
-  
-        <div className="flex-1 flex flex-col">
-          <Header />
-  
-          <main className="flex-1 p-4 bg-white">
-            <QuizAttempt id={id} />
-          </main>
-        </div>
-      </div>
-    );
+  const { id } = params;
+
+  // Define dummy functions for goBack and goToNextStep to pass them as props.
+  const goBack = () => {
+    console.log("Going back");
   };
-  
-  export default EditPage;
-  
+
+  const goToNextStep = () => {
+    console.log("Going to the next step");
+  };
+
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-4 bg-white">
+          {/* Pass quizId, goBack, and goToNextStep props to QuizAttempt */}
+          <QuizAttempt quizId={id} goBack={goBack} goToNextStep={goToNextStep} />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default EditPage;
