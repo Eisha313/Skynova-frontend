7e67f52f8b101b77ecbfebc364827036afe94b65
Eisha313 <eishakamran313@gmail.com>
2024-09-30 20:35:52 +0500

    pushing code5

--- src/app/userRender/verbal/[id]/attempt/page.tsx
+++ src/app/userRender/verbal/[id]/attempt/page.tsx
@@ -1,18 +1,46 @@
 
 
+// import React from 'react';
+// import Header from '@/app/components/header';
+// import Sidebar from '@/app/components/sidebarDashboard';
+// import QuizAttempt from '@/app/userComponents/verbal/QuizAttempt';
+
+// interface EditPageProps {
+//   params: {
+//     id: string;
+//   };
+//   goToNextStep?: () => void; 
+// }
+
+// const EditPage = ({ params, goToNextStep }: EditPageProps) => {
+//   const { id } = params;
+
+//   return (
+//     <div className="flex h-screen">
+//       <Sidebar />
+//       <div className="flex-1 flex flex-col">
+//         <Header />
+//         <main className="flex-1 p-4 bg-white">
+//           <QuizAttempt quizId={id} goToNextStep={goToNextStep} /> 
+//         </main>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EditPage;
 import React from 'react';
 import Header from '@/app/components/header';
 import Sidebar from '@/app/components/sidebarDashboard';
 import QuizAttempt from '@/app/userComponents/verbal/QuizAttempt';
 
-interface EditPageProps {
+interface PageProps {
   params: {
     id: string;
   };
-  goToNextStep?: () => void; // Optional prop for stepper
 }
 
-const EditPage = ({ params, goToNextStep }: EditPageProps) => {
+const EditPage = ({ params }: PageProps) => {
   const { id } = params;
 
   return (
@@ -21,7 +49,7 @@ const EditPage = ({ params, goToNextStep }: EditPageProps) => {
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="flex-1 p-4 bg-white">
-          <QuizAttempt quizId={id} goToNextStep={goToNextStep} /> 
+          <QuizAttempt quizId={id} /> {/* Removed goToNextStep */}
         </main>
       </div>
     </div>
