d63afc8fe5f473223160d88f553016653358b9ed
Eisha313 <eishakamran313@gmail.com>
2024-11-09 21:33:35 +0500

    code workkkkk777

--- src/app/userRender/nonverbal/[id]/result/page.tsx
+++ src/app/userRender/nonverbal/[id]/result/page.tsx
@@ -1,22 +1,47 @@
+// import React from 'react';
+// import Header from '@/app/components/LandingPage/header';
+
+// import NonVerbalQuizResult from '@/app/userComponents/nonverbal/quizResult';
+
+// const ResultPage = ({ params }: { params: { id: string } }) => {
+//     const { id } = params;
+//     console.log('userTitle', id);
+  
+//     return (
+//         <div className='flex-1 flex flex-col'>
+//         <Header/>
+//         <main className="flex-1 p-4 bg-gray h-screen overflow-auto ">
+//         <NonVerbalQuizResult id={id}/>
+//         </main>
+       
+//         </div>
+//     );
+//   };
+//   export default ResultPage;
+  
 import React from 'react';
 import Header from '@/app/components/LandingPage/header';
-
 import NonVerbalQuizResult from '@/app/userComponents/nonverbal/quizResult';
 
 const ResultPage = ({ params }: { params: { id: string } }) => {
-    const { id } = params;
-    console.log('userTitle', id);
-  
-    return (
-        <div className='flex-1 flex flex-col'>
-        <Header/>
-        <main className="flex-1 p-4 bg-gray h-screen overflow-auto ">
-        <NonVerbalQuizResult id={id}/>
-        </main>
-       
-        </div>
-    );
+  const { id } = params;
+  console.log('userTitle', id);
+
+  // Define the goBackToList function
+  const goBackToList = () => {
+    // Redirect to the previous page or quiz list
+    window.history.back();
   };
-  export default ResultPage;
-  
-  
+
+  return (
+    <div className='flex-1 flex flex-col'>
+      <Header />
+      <main className="flex-1 p-4 bg-gray h-screen overflow-auto ">
+        {/* Pass both id and goBackToList to NonVerbalQuizResult */}
+        <NonVerbalQuizResult id={id} goBackToList={goBackToList} />
+      </main>
+    </div>
+  );
+};
+
+export default ResultPage;
